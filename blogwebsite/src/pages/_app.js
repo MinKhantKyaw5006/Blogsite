3bc16b200de9c7584375dd733eec141b73a153f5
MinKhantKyaw <minkhant050603@gmail.com>
2024-04-13 03:09:40 +0700

    Update _app.js

--- blogwebsite/src/pages/_app.js
+++ blogwebsite/src/pages/_app.js
@@ -1,10 +1,10 @@
-// import React from 'react';
-// // import '../path/to/SearchBar.css'; // Import your global CSS file(s)
-// import SearchBar from '@/Home/HomeComponent/SearchBar';
+import React from 'react';
+// import '../path/to/SearchBar.css'; // Import your global CSS file(s)
+import SearchBar from '@/Home/HomeComponent/SearchBar';
 
-// function MyApp({ Component, pageProps }) {
-//     return <Component {...pageProps} />;
-//   }
+function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+  }
   
-//   export default MyApp;
+  export default MyApp;
   
